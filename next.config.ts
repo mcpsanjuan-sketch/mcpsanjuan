0c78266d2b1f594d2dc3ee6fcf442f1c6cde4d83
mcpsanjuan-sketch <mcpsanjuan@gbox.ncf.edu.ph>
2025-12-17 22:29:14 +0800

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: [
+    "http://192.168.254.102:3000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+  ],
 };
 
-export default nextConfig;
+module.exports = nextConfig;
